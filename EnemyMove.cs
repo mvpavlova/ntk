ee3ddf3d5a5e8a3cbc5bdeb98abec73fd37ddc3e
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2019-08-30 11:08:45 +0300

    Add files via upload

---
+++ EnemyMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;  // скорость движения
+    [SerializeField] private GameObject[] myWaypoints;  // список точек по которым будет двигаться енеми
+
+    private int myWaypointId = 0;                    // текущая точка в массиве куда двигаться
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    void EnemyMovement()
+    {
+        // если точки есть
+        if (myWaypoints.Length != 0)
+        {
+            // если мы уже достигли назначенной точки, то переходим к следующей
+            if (Vector3.Distance(myWaypoints[myWaypointId].transform.position, transform.position) <= 0)
+            {
+                myWaypointId++;
+            }
+
+            //если точки исчерпаны то переходим к началу массива точек
+            if (myWaypointId >= myWaypoints.Length)
+            {
+                myWaypointId = 0;
+            }
+
+            //движемся в назначенную точку
+            // move towards waypoint
+            transform.position = Vector3.MoveTowards(transform.position, myWaypoints[myWaypointId].transform.position, moveSpeed * Time.deltaTime);
+        }
+    }
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+    }
+}
---
+++ newfunction.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newfunction : MonoBehaviour
+{
+    private Animation anim;
+    private bool anima = false;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+
+    }
+
+    public void Die()
+    {
+        if (anima == true)
+            return;
+
+        anima = true;
+        anim.Play("Bear_die");
+        Invoke("ResetAnimation", 1.1f);
+    }
+    public void Attack()
+    {
+        if (anima == true)
+            return;
+
+        anima = true;
+        anim.Play("Bear_attack");
+        Invoke("ResetAnimation", 2.1f);
+    }
+    
+    public void Walk()
+    {
+        if (anima == true)
+            return;
+
+        anima = true;
+        anim.Play("Bear_walk");
+        Invoke("ResetAnimation", 1.0f);
+    }
+
+    public void ResetAnimation()
+    {
+        anim.Play("Bear_idle");
+        anima = false;
+
+    }
+    
+  
+}
